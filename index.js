4e755b1e7950c757e4c03ea524157633b60b8776
ProtoJanus <165827599+ProtoJanus@users.noreply.github.com>
2024-10-25 14:37:05 -0500

    Add game functionality to play five rounds and determine winner

--- index.js
+++ index.js
@@ -11,15 +11,48 @@ function getUserChoice() {
 }
 
 function playRound(userChoice, computerChoice) {
-    if (userChoice === 'paper' && computerChoice === 'rock' || computerChoice === 'scissors') {
-
-    } elseif(userChoice === '')
+    // tie = 0, human win = 1, computer wins = 2
+    console.log(userChoice);
+    console.log(computerChoice);
+    // checks if user wins
+    if (userChoice === 'paper' && computerChoice === 'rock' || userChoice === 'rock' && computerChoice === 'scissors' || userChoice === 'scissors' && computerChoice === 'paper') {
+        return 1;
+        // checks if computer wins
+    } else if (computerChoice != userChoice) {
+        return 2;
+        // all else fails must be a tie
+    } else {
+        return 0;
+    }
 }
-let computerScore = 0;
-let humanScore = 0;
 
-const humanSelection = getUserChoice();
-const computerSelection = getComputerChoice();
+function playGame() {
+    let computerScore = 0;
+    let humanScore = 0;
 
-playRound(humanSelection, computerSelection);
+    for (let i = 0; i < 5; i++) {
+        const humanSelection = getUserChoice();
+        const computerSelection = getComputerChoice();
 
+        let result = playRound(humanSelection, computerSelection);
+
+        if (result === 0) {
+            console.log("You tied with the computer!");
+        } else if (result === 1) {
+            console.log("You won!");
+            humanScore += 1;
+        } else {
+            console.log("You Lost!");
+            computerScore += 1;
+        }
+    }
+
+    console.log(`Final Score\nComputer: ${computerScore}\nPlayer: ${humanScore}`)
+    if (humanScore > computerScore) {
+        console.log("You won the game!");
+    } else {
+        console.log("You lost the game!");
+    }
+}
+
+playGame();
