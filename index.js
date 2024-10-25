20aa47f1f72510e75e356fb20a2b78cd45583b00
ProtoJanus <165827599+ProtoJanus@users.noreply.github.com>
2024-10-25 13:35:52 -0500

    Add logic for playing one round of paper rock scissors

--- index.js
+++ index.js
@@ -4,3 +4,22 @@ function getComputerChoice() {
     let choices = ["rock", "paper", "scissors"];
     return choices[Math.floor(Math.random() * choices.length)];
 }
+
+function getUserChoice() {
+    let choice = prompt("Enter either Rock, Paper, or Scissors: ").toLowerCase();
+    return choice;
+}
+
+function playRound(userChoice, computerChoice) {
+    if (userChoice === 'paper' && computerChoice === 'rock' || computerChoice === 'scissors') {
+
+    } elseif(userChoice === '')
+}
+let computerScore = 0;
+let humanScore = 0;
+
+const humanSelection = getUserChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
